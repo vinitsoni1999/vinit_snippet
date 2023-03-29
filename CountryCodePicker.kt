f8cc8953867fb33edf4ee80a12a77c9af549ae99
vinitsoni1999 <125573053+vinitsoni1999@users.noreply.github.com>
2023-03-29 15:32:41 +0530

    Create CountryCodePicker.kt
    If you are looking for an android library for Country Selector or Country Spinner or Country Phone Code selector, this is the perfect place for you.
    Country Code Picker (CCP)  is an android library which provides an easy way to search and select country or country phone code for the telephone number.
    Give a professional touch to your well designed form like login screen, sign up screen, edit profile screen with CCP. When used as phone code picker, it helps by removing confusion about how to add phone number and making view more understandable.
    With CCP you can get following views easily without boilerplate code. (Left: Phone code selector. Right: Country Selector)

---
+++ CountryCodePicker.kt
@@ -0,0 +1,15 @@
+//XML FILE
+ <com.hbb20.CountryCodePicker
+    android:layout_width="wrap_content"
+    android:layout_height="wrap_content"
+    android:backgroundTint="@color/black"
+    app:ccp_showFlag="false"
+    app:ccp_showNameCode="false"
+    android:gravity="center"
+    app:ccp_textSize="16dp"
+    style="@style/txtCountryCode"
+    app:ccpDialog_textColor="@color/black"
+    />
+
+  //dependency
+    implementation 'com.hbb20:ccp:2.5.1'
